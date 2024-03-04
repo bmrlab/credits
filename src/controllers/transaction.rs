b81dfa5b0d14e48e443f198810cff9ca62f99a69
ouyangkang <ouyangkang@tezign.com>
2024-03-04 11:17:43 +0800

    补充交易逻辑

--- src/controllers/transaction.rs
+++ src/controllers/transaction.rs
@@ -1,7 +1,9 @@
 #![allow(clippy::unused_async)]
 use axum::http::StatusCode;
 use loco_rs::{controller::ErrorDetail, prelude::*};
-use sea_orm::{ColumnTrait, ConnectionTrait, QueryFilter, Statement, TransactionTrait};
+use sea_orm::{
+    prelude::Decimal, ColumnTrait, ConnectionTrait, QueryFilter, Statement, TransactionTrait,
+};
 use serde_json::json;
 
 use crate::{
@@ -11,10 +13,6 @@ use crate::{
     },
     views::transaction::{TransItem, TransactionResp},
 };
-
-pub async fn echo(req_body: String) -> String {
-    req_body
-}
 
 /*
 
@@ -30,120 +28,169 @@ pub async fn api_exec_trans(
     State(ctx): State<AppContext>,
     Json(params): Json<TransItem>,
 ) -> Result<Json<TransactionResp>> {
+    tracing::info!("发起交易 params: {:?}", &params);
     let params_clone = params.clone();
     let from_addr = params_clone.from_addr;
     let to_addr = params_clone.to_addr;
     let amount = params_clone.amount;
-    let direction = transaction_event_type::get_direction(&params_clone.event_type);
+    let events_type = transaction_event_type::split_complex_event(&params_clone.event_type);
     let event_id = get_uuid();
+    tracing::info!(
+        "交易开始 event_id: {} events_type: {:?}",
+        &event_id,
+        &events_type
+    );
+    for ele in events_type {
+        let direction = transaction_event_type::get_direction(ele);
 
-    loop {
-        // 1.获取两个钱包的信息。
-        // let tran = params.new(&event_id);
-        let from_wallet = Wallets::find()
-            .filter(wallets::Column::Addr.eq(&from_addr))
-            .one(&ctx.db)
-            .await?;
+        loop {
+            // 1.获取两个钱包的信息。
+            // let tran = params.new(&event_id);
+            let from_wallet = Wallets::find()
+                .filter(wallets::Column::Addr.eq(&from_addr))
+                .one(&ctx.db)
+                .await?;
 
-        let to_wallet = Wallets::find()
-            .filter(wallets::Column::Addr.eq(&to_addr))
-            .one(&ctx.db)
-            .await?;
+            let to_wallet = Wallets::find()
+                .filter(wallets::Column::Addr.eq(&to_addr))
+                .one(&ctx.db)
+                .await?;
 
-        // 2.判断两个钱包余额是否支持交易，不支持，交易失败，记录事件交易信息。交易结束。
-        let mut transaction_active: transaction_events::ActiveModel =
-            build_transaction(&from_wallet, &to_wallet, &params, direction);
-        transaction_active.event_id = Set(event_id.clone());
-        let state_value = transaction_active.clone().state.as_ref().clone();
+            // 2.判断两个钱包余额是否支持交易，不支持，交易失败，记录事件交易信息。交易结束。
+            let mut transaction_active: transaction_events::ActiveModel =
+                build_transaction(&from_wallet, &to_wallet, &params, direction);
+            transaction_active.event_id = Set(event_id.clone());
+            let state_value = transaction_active.clone().state.as_ref().clone();
 
-        let msg: String = transaction_active
-            .clone()
-            .status_msg
-            .as_ref()
-            .clone()
-            .ok_or_else(|| Error::NotFound)?;
-        if state_value < 0 {
-            transaction_active.insert(&ctx.db).await?;
-            return Err(Error::CustomError(
-                StatusCode::BAD_REQUEST,
-                ErrorDetail::new("bad_request", &msg),
-            ));
-        }
+            if state_value < 0 {
+                let msg: String = transaction_active
+                    .clone()
+                    .status_msg
+                    .as_ref()
+                    .clone()
+                    .ok_or_else(|| Error::NotFound)?;
+                transaction_active.insert(&ctx.db).await?;
+                return Err(Error::CustomError(
+                    StatusCode::BAD_REQUEST,
+                    ErrorDetail::new("bad_request", &msg),
+                ));
+            }
 
-        // 3.钱包进行计算逻辑。
-        let from_wallet_model = from_wallet.ok_or_else(|| Error::NotFound)?;
-        let mut from_balance = from_wallet_model.balance.clone();
+            // 3.钱包进行计算逻辑。
+            let from_wallet_model = from_wallet.ok_or_else(|| Error::NotFound)?;
+            let mut from_balance = from_wallet_model.balance.clone();
 
-        let to_wallet_model = to_wallet.ok_or_else(|| Error::NotFound)?;
-        let mut to_balance = to_wallet_model.balance.clone();
+            let to_wallet_model = to_wallet.ok_or_else(|| Error::NotFound)?;
+            let mut to_balance = to_wallet_model.balance.clone();
 
-        if direction == PAYMENT {
-            from_balance -= amount;
-            to_balance += amount;
-        } else if direction == INCOME {
-            from_balance += amount;
-            to_balance -= amount;
-        }
+            if direction == PAYMENT {
+                from_balance -= amount;
+                to_balance += amount;
+            } else if direction == INCOME {
+                from_balance += amount;
+                to_balance -= amount;
+            }
 
-        // 4.开始事务：更新钱包余额条件加上原有钱包余额，两个钱包更新余额成功，则提交事务，否则事务回滚，进行交易重试，从1开始
-        let txn = ctx.db.begin().await?;
-        let from_res = &txn
-            .execute(Statement::from_sql_and_values(
-                sea_orm::DatabaseBackend::MySql,
-                "update wallets set balance = ? where addr = ? and balance = ?",
-                [
-                    from_balance.into(),
-                    from_addr.clone().into(),
-                    from_wallet_model.balance.into(),
-                ],
-            ))
-            .await?;
-        let to_res = &txn
-            .execute(Statement::from_sql_and_values(
-                sea_orm::DatabaseBackend::MySql,
-                "update wallets set balance = ? where addr = ? and balance = ?",
-                [
-                    to_balance.into(),
-                    to_addr.clone().into(),
-                    to_wallet_model.balance.into(),
-                ],
-            ))
-            .await?;
-        if from_res.rows_affected().eq(&1) && to_res.rows_affected().eq(&1) {
-            txn.commit().await?;
-            // 5.事件交易信息，两个钱包的账单信息存储。
-            transaction_active.state = Set(10);
-            let transaction_event_type = transaction_active.insert(&ctx.db).await?;
-            println!("transaction_event_type {:?}", transaction_event_type);
-            // 6.交易结束。
-            break;
-        } else {
-            txn.rollback().await?
+            // 4.开始事务：更新钱包余额条件加上原有钱包余额，两个钱包更新余额成功，则提交事务，否则事务回滚，进行交易重试，从1开始
+            let txn = ctx.db.begin().await?;
+            let from_res = &txn
+                .execute(Statement::from_sql_and_values(
+                    sea_orm::DatabaseBackend::MySql,
+                    "update wallets set balance = ? where addr = ? and balance = ?",
+                    [
+                        from_balance.into(),
+                        from_addr.clone().into(),
+                        from_wallet_model.balance.into(),
+                    ],
+                ))
+                .await?;
+            let to_res = &txn
+                .execute(Statement::from_sql_and_values(
+                    sea_orm::DatabaseBackend::MySql,
+                    "update wallets set balance = ? where addr = ? and balance = ?",
+                    [
+                        to_balance.into(),
+                        to_addr.clone().into(),
+                        to_wallet_model.balance.into(),
+                    ],
+                ))
+                .await?;
+            if from_res.rows_affected().eq(&1) && to_res.rows_affected().eq(&1) {
+                tracing::info!("钱包积分转账成功 event_id: {}", &event_id);
+                txn.commit().await?;
+                // 5.事件交易信息，两个钱包的账单信息存储。
+                transaction_active.state = Set(10);
+                transaction_active.insert(&ctx.db).await?;
+                let bill_actives = build_bill_actives(
+                    &event_id,
+                    &from_addr,
+                    from_wallet_model.balance.clone(),
+                    &to_addr,
+                    to_wallet_model.balance.clone(),
+                    amount,
+                    ele,
+                    json!({}),
+                );
+
+                for bill in bill_actives {
+                    bill.insert(&ctx.db).await?;
+                }
+                // 6.交易结束。
+                tracing::info!("交易成功 event_id: {}", &event_id);
+                break;
+            } else {
+                tracing::info!("交易失败回滚 event_id: {}", &event_id);
+                txn.rollback().await?
+            }
         }
     }
-
-    // let mut event_ids: Vec<String> = vec![];
-
-    // for item in &params.trans {
-    //     let event_id = get_uuid();
-    //     let active_mode = item.new(&event_id);
-    //     event_ids.push(event_id.clone());
-    //     active_mode.insert(&ctx.db).await?;
-    // }
-
-    // let pool = &ctx.redis.ok_or_else(|| Error::NotFound)?.clone();
-    // let mut pool_coon = pool.get().await?;
-    // let redis_coon = pool_coon.unnamespaced_borrow_mut();
-    // let msg = json!(&event_ids).to_string();
-    // tracing::info!("redis lpush msg={}", &msg);
-    // redis_coon.lpush(QUEUE_KEY, msg).await?;
-
+    tracing::info!("交易结束 event_id: {}", &event_id);
     format::json(TransactionResp::new(event_id))
 }
 
 fn get_uuid() -> String {
     let uuid = uuid::Uuid::new_v4();
     uuid.to_string()
+}
+
+fn build_bill_actives(
+    event_id: &str,
+    from_addr: &str,
+    from_addr_amount: Decimal,
+    to_addr: &str,
+    to_addr_amount: Decimal,
+    amount: Decimal,
+    event_type: &str,
+    info: serde_json::Value,
+) -> Vec<bills::ActiveModel> {
+    let mut bill_actives = vec![];
+    let from_bill = bills::ActiveModel {
+        event_id: Set(event_id.to_string()),
+        from_addr: Set(Some(from_addr.to_string())),
+        to_addr: Set(Some(to_addr.to_string())),
+        amount: Set(amount.clone()),
+        current_balance: Set(from_addr_amount.clone()),
+        event_type: Set(event_type.to_string()),
+        direction: Set(transaction_event_type::get_direction(event_type)),
+        info: Set(Some(info.clone())),
+        ..Default::default()
+    };
+    let to_bill = bills::ActiveModel {
+        event_id: Set(event_id.to_string()),
+        from_addr: Set(Some(to_addr.to_string())),
+        to_addr: Set(Some(from_addr.to_string())),
+        amount: Set(amount.clone()),
+        current_balance: Set(to_addr_amount.clone()),
+        event_type: Set(event_type.to_string()),
+        direction: Set(transaction_event_type::get_opposite_direction(
+            transaction_event_type::get_direction(event_type),
+        )),
+        info: Set(Some(info.clone())),
+        ..Default::default()
+    };
+    bill_actives.push(from_bill);
+    bill_actives.push(to_bill);
+    bill_actives
 }
 
 fn build_transaction(
@@ -154,6 +201,7 @@ fn build_transaction(
 ) -> transaction_events::ActiveModel {
     let mut tran_mode_active: transaction_events::ActiveModel = param.new();
     tran_mode_active.direction = Set(direction);
+    tran_mode_active.state = Set(0);
 
     let from_addr = &param.from_addr;
     let to_addr = &param.to_addr;
@@ -207,68 +255,8 @@ fn build_transaction(
     tran_mode_active
 }
 
-// pub async fn hello(State(ctx): State<AppContext>) -> Result<String> {
-//     let pool = &ctx.redis.ok_or_else(|| Error::NotFound)?.clone();
-//     let mut pool_coon = pool.get().await?;
-//     let coon = pool_coon.unnamespaced_borrow_mut();
-
-//     let kang: String = coon.set_ex("kang", "bbb", 1000).await?;
-//     println!("kang = {}", &kang);
-//     coon.lpush("aa", "1").await?;
-//     coon.lpush("aa", "1").await?;
-
-//     let len: i32 = coon.llen("aa").await?;
-//     println!(" len {}", len);
-
-//     loop {
-//         let temp = pool_coon.unnamespaced_borrow_mut();
-//         let value: Option<redis::Value> =
-//             temp.rpop("aa", Some(NonZeroUsize::new(1).unwrap())).await?;
-
-//         if let Some(redis::Value::Bulk(values)) = value {
-//             println!("Bulk values:");
-//             for v in values {
-//                 match v {
-//                     redis::Value::Data(data) => {
-//                         println!("  Binary data: {}", String::from_utf8_lossy(&data))
-//                     }
-//                     _ => println!("  Other value: {:?}", v),
-//                 }
-//             }
-//         } else {
-//             println!("Option is None");
-//         }
-
-//         // if let Some(value) = cc {
-//         //     println!("cc value={}", value);
-//         // } else {
-//         //     println!("cc is null");
-//         // }
-//         let next: i32 = temp.llen("aa").await?;
-//         println!(" loop len ={}", next);
-//         if next == 0 {
-//             break;
-//         }
-//     }
-
-//     // let cmd = coon.cmd_with_key("get", "kang".to_string());
-
-//     // let res = cmd.query_async(coon.unnamespaced_borrow_mut());
-
-//     // let res = coon
-//     //     .set_nx_ex("kang".to_string(), "1".to_string(), 10)
-//     //     .await;
-//     // let value = res.unwrap();
-
-//     // println!("value {:?}", value);
-
-//     // do something with context (database, etc)
-//     format::text("hello")
-// }
-
 pub fn routes() -> Routes {
     Routes::new()
         .prefix("transaction")
         .add("/", post(api_exec_trans))
-        .add("/echo", post(echo))
 }
